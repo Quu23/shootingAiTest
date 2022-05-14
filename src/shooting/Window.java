dedf840fc2fcfe446f0e1579d445f17712342652
Quu <91326783+Quu23@users.noreply.github.com>
2022-05-14 21:57:16 +0900

    画面表示は完了。あとはdraw関数の定義

--- src/shooting/Window.java
+++ src/shooting/Window.java
@@ -1,12 +1,34 @@
 package shooting;
 
+
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Window extends Application{
-    @Override
-    public void start(Stage arg0) throws Exception {
-        
-    }
 
+    GraphicsContext g ;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Group root =  new Group();
+
+        Canvas canvas = new Canvas(500,500);
+        g=canvas.getGraphicsContext2D();
+
+        root.getChildren().add(canvas);
+
+        stage.setTitle("Shooting");
+        Scene scene = new Scene(root,500,500,Color.WHITE);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
