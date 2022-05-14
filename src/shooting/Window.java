d49e40a4523b03671b15328332cdff322be29b3c
Quu <91326783+Quu23@users.noreply.github.com>
2022-05-14 21:23:20 +0900

    first commit

---
+++ src/shooting/Window.java
@@ -0,0 +1,12 @@
+package shooting;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Window extends Application{
+    @Override
+    public void start(Stage arg0) throws Exception {
+        
+    }
+
+}
