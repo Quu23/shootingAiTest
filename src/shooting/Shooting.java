defbd802a8e866553a9fb69c681d23f9b72c4a8c
Quu <91326783+Quu23@users.noreply.github.com>
2022-05-15 20:58:36 +0900

    mainクラスの定義

--- src/shooting/Shooting.java
+++ src/shooting/Shooting.java
@@ -1,5 +1,11 @@
 package shooting;
 
 public class Shooting {
-    
+
+    public static void main(String[] args) {
+        Player player = new Player(100, 200);
+        Enemy enemy = new Enemy();
+        Window.run(args,player,enemy);
+
+    }
 }
